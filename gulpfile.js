ad309a933eb699992c5f207e7cb47d81d1e65cd7
Tim McNamara <timlmcn@gmail.com>
2018-12-17 22:34:49 +1100

    update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,69 @@
-const gulp = require('gulp');
-var browserSync = require('browser-sync').create(); 
-const sass = require('gulp-sass');
-const imagemin = require('gulp-imagemin');
-const ufligy = require('gulp-uglify');
-
+const gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+var clean = require("gulp-clean");
+const htmlmin = require("gulp-htmlmin");
 
 // Watch & Serve
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-        server: './src'
-    });
+gulp.task("serve", ["sass"], function() {
+  browserSync.init({
+    server: "./src"
+  });
 
-    gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['sass']);    
-    gulp.watch(['src/*.html', 'src/js/*.js']).on('change', browserSync.reload);
+  gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]);
+  gulp.watch(["src/*.html", "src/js/*.js"]).on("change", browserSync.reload);
 });
 
 // Compile SCSS
-gulp.task('sass', function() {
-    return gulp.src('src/scss/*.scss')
-        .pipe(sass())    
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src("src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("build/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve', 'sass'])
+// build tasks
+
+gulp.task("build:html", function() {
+  return gulp
+    .src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build:css", function() {
+  return gulp
+    .src("src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./build/css"));
+});
+
+gulp.task("build:js", function() {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build:imagemin", function() {
+  return gulp
+    .src("src/assets/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("build/assets"));
+});
+
+gulp.task("build:clean", function() {
+  return gulp.src("./build").pipe(clean());
+});
+
+gulp.task("default", ["serve", "sass"]);
+gulp.task("build", [
+  "build:clean",
+  "build:html",
+  "build:css",
+  "build:js",
+  "build:imagemin"
+]);
